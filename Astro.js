00e4a625bb69f868ebdb506f2a1598ca1d325cba
Linad-Lemonad <70460987+Linad-Lemonad@users.noreply.github.com>
2023-11-30 19:00:18 +0300

    Update Astro.js

--- Astro.js
+++ Astro.js
@@ -427,7 +427,7 @@
         game.navigation.gamePage.playerUI.toolbarController.inventory.element.appendChild(colorDetector_Modal);
 
         window.game.FAPI.registerMod("zero.astro", (mod) => {
-            window.game.FAPI.registerArrows([tFlipFlopSplit1, tFlipFlopSplit2, tFlipFlopSplit3], mod);
+            window.game.FAPI.registerArrows([tFlipFlopSplit1, tFlipFlopSplit2, tFlipFlopSplit3, tFlipFlopSplit4], mod);
             window.game.FAPI.registerArrows([diagonalSplit1, diagonalSplit2, diagonalSplit3, diagonalSplit4, blueBlocker], mod);
             window.game.FAPI.registerArrows([diagonalDetector, doubleAnd, colorDetector], mod);
             window.game.FAPI.registerArrows([arrow9, adder, block], mod);
